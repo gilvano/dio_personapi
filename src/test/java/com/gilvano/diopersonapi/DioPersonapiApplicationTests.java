951267d1ff27ccfa2f5a82cdedcf2efaf1ad435e
Gilvano Piontkoski <g.piontkoski@gmail.com>
2021-10-18 13:39:48 -0300

    Criar projeto

---
+++ src/test/java/com/gilvano/diopersonapi/DioPersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilvano.diopersonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioPersonapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
