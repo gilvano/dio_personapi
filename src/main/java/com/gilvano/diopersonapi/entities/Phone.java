a36bf98aec003942f6c293a627fed27ba739bfbb
Gilvano Piontkoski <g.piontkoski@gmail.com>
2021-10-18 15:08:23 -0300

    Criar entidades

---
+++ src/main/java/com/gilvano/diopersonapi/entities/Phone.java
@@ -0,0 +1,22 @@
+package com.gilvano.diopersonapi.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false, unique = true)
+    private String number;
+}
